a796f1d5f40b1c40a176112dccc6958418b60cbc
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-07 14:10:49 +0530

    final config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from "react";
 import { render } from "react-dom";
+import HomePage from "./Homepage";
 
 const App: FunctionComponent = () => {
   return (
     <React.StrictMode>
       <div>
         <h3>React is setup !</h3>
+        <HomePage />
       </div>
     </React.StrictMode>
   );
---
+++ src/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Netflix homepage</h2>
+    </div>
+  );
+};
+
+export default HomePage;
