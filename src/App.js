63fb5de2c4952ca2cc2bd98de152804d98412778
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-07 13:23:32 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h3>React is setup !</h3>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
