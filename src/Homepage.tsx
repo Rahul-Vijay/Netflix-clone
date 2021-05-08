dc8c2e1fc52a9b6b782e806627249b09f4486fd7
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-08 08:11:46 +0530

    refactor

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,30 +1,10 @@
 import React from "react";
-import Header from "./Header";
+import TopContent from "./Top-Content";
 
 const HomePage = () => {
   return (
-    <div className="main-container">
-      <Header />
-      {/* content div */}
-      <div className="content-container">
-        <div>
-          <div>Unlimited movies, TV shows and more.</div>
-          <div>Watch anywhere. Cancel anytime.</div>
-          <div>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </div>
-          <div className="sub-content-email">
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Email Address"
-            />
-            <button className="rd-btn started">Get started</button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <TopContent />
     </div>
   );
 };
