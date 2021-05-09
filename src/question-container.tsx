83a3b5c6eb84b92ced2dabeced8981084f9c4805
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-09 10:57:51 +0530

    faqs done

--- src/question-container.tsx
+++ src/question-container.tsx
@@ -9,6 +9,8 @@ interface IProps {
 const QuestionContainer: FunctionComponent<IProps> = (props: IProps) => {
   const { ques, ans, id } = props;
   const idString = `${id}-ans`;
+  const idX = `${id}-x`;
+  const idPlus = `${id}-plus`;
 
   const setRestToNone = () => {
     const nList = document.querySelectorAll(
@@ -18,7 +20,19 @@ const QuestionContainer: FunctionComponent<IProps> = (props: IProps) => {
       for (let z = 0; z < nList.length; z++) {
         if (nList[z].id !== idString) {
           document.getElementById(nList[z].id)!.style.display = "none";
-        } else document.getElementById(nList[z].id)!.style.display = "block";
+          document.getElementById(`${z}-x`)!.style.display = "block";
+          document.getElementById(`${z}-plus`)!.style.display = "none";
+        } else {
+          if (document.getElementById(nList[z].id)!.style.display === "block") {
+            document.getElementById(nList[z].id)!.style.display = "none";
+            document.getElementById(`${z}-plus`)!.style.display = "none";
+            document.getElementById(`${z}-x`)!.style.display = "block";
+          } else {
+            document.getElementById(nList[z].id)!.style.display = "block";
+            document.getElementById(`${z}-plus`)!.style.display = "block";
+            document.getElementById(`${z}-x`)!.style.display = "none";
+          }
+        }
       }
     }
   };
@@ -33,14 +47,12 @@ const QuestionContainer: FunctionComponent<IProps> = (props: IProps) => {
       <div>
         <div> {ques} </div>
         <div>
-          {/* <span
-            style={
-              display === "block" ? { display: "none" } : { display: "block" }
-            }
-          >
+          <span id={idX} style={{ display: "block" }}>
             +
-          </span>{" "}
-          <span style={{ display }}>x</span> */}
+          </span>
+          <span id={idPlus} style={{ display: "none" }}>
+            x
+          </span>
         </div>
       </div>
       <div id={idString}>{ans}</div>
