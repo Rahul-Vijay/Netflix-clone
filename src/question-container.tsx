716761f1928dd7f9bfc8e7b6bce7cace32eb6fc0
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-09 09:46:24 +0530

    onclick

--- src/question-container.tsx
+++ src/question-container.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 interface IProps {
   ques: String;
@@ -9,15 +9,25 @@ interface IProps {
 const QuestionContainer: FunctionComponent<IProps> = (props: IProps) => {
   const { ques, ans, id } = props;
   const idString = `${id}-ans`;
+  const [display, setDisplay] = useState("none");
+
   return (
-    <div className="question-container">
+    <div
+      className="question-container"
+      onClick={() => {
+        if (display === "block") setDisplay("none");
+        else setDisplay("block");
+      }}
+    >
       <div>
         <div> {ques} </div>
         <div>
           <span>+</span> <span>x</span>
         </div>
       </div>
-      <div id={idString}>{ans}</div>
+      <div id={idString} style={{ display }}>
+        {ans}
+      </div>
     </div>
   );
 };
