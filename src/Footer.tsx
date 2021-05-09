6f0e22e02601a4bfbc3cfd2e4198fb7eea8c60ae
Rahul_Vijay <vk.rahul318@gmail.com>
2021-05-09 13:18:19 +0530

    arrangement

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,8 +2,23 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div>This is the footer</div>
+    <div style={{ backgroundColor: "black" }}>
+      <div className="footer">
+        <div>Questions? Call 000-800-040-1843 </div>
+        <div className="footer-links">
+          <ul>
+            <li>FAQ</li>
+            <li>Account</li>
+            <li>Investor Relation</li>
+            <li>Ways to Watch</li>
+            <li>Privacy</li>
+          </ul>
+        </div>
+        <div style={{ display: "inline-block", marginRight: "20px" }}>
+          <button className="lg-btn"> English </button>
+        </div>
+        <div>Netflix India</div>
+      </div>
     </div>
   );
 };
